c8f289199b73194238c0f4e880ba4dbd8a063bb3
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2022-02-12 17:42:00 -0300

    Initial commit

---
+++ src/main/java/coffeeapi/com/ney/CoffeeapiApplication.java
@@ -0,0 +1,13 @@
+package coffeeapi.com.ney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coffeeapi/com/ney/CoffeeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package coffeeapi.com.ney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
