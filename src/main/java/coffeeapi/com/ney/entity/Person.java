3c09676483b5e5f733b254424dfdee7d8e4d1bd3
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2022-02-12 19:46:34 -0300

    Create Person.java

---
+++ src/main/java/coffeeapi/com/ney/entity/Person.java
@@ -0,0 +1,35 @@
+package coffeeapi.com.ney.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	 
+	@Column(nullable = false, unique = true)
+	private String CPF;
+	
+	@Column(nullable = false, unique = true)
+	private String food;
+	
+
+}
