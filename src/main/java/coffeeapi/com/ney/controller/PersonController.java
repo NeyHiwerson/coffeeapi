f3bcadc30cbe4b4714fbaa751d8b15ad55739cdc
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2022-02-12 17:59:58 -0300

    Create PersonController.java

---
+++ src/main/java/coffeeapi/com/ney/controller/PersonController.java
@@ -0,0 +1,16 @@
+package coffeeapi.com.ney.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+	
+	@GetMapping
+	public String getAllpeople() {
+		return "API Rest";
+	}
+
+}
